4809e705392069534fdee1a73da1f2715574d382
Wai-Lun Lin <wlin@kencove.com>
2026-01-26 15:45:15 -0500

    feat: add staging v2 tax endpoint

--- account_avatax_oca/models/avalara_salestax.py
+++ account_avatax_oca/models/avalara_salestax.py
@@ -43,6 +43,7 @@ class AvalaraSalestax(models.Model):
             ("https://sandbox-rest.avatax.com/api/v2", "Sandbox (REST API)"),
             ("https://tax.gc.kencove.com", "Kencove Tax (REST API)"),
             ("https://tax-staging.gc.kencove.com", "Kencove Tax Staging (REST API)"),
+            ("https://tax-v2-staging.gc.kencove.com", "Kencove Tax Staging V2 (REST API)"),
         ],
         string="Service URL",
         default="https://rest.avatax.com/api/v2",
